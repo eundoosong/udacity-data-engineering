224f818862d6a77b05939839fe65497e4167ed2f
Eundoo Song <eundoo.song@gmail.com>
2020-11-22 04:40:28 +0900

    Remove dummy operator

--- 5.data-pipeline/airflow/plugins/operators/dummy_operator.py
+++
@@ -1,9 +0,0 @@
-from airflow.models import BaseOperator
-
-
-class DummyOperator(BaseOperator):
-    def __init__(self, *args, **kwargs):
-        super(DummyOperator, self).__init__(*args, **kwargs)
-
-    def execute(self, context):
-        pass
