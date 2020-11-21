9fb66b5f3096ca3db196380141ed5cdff105e841
Eundoo Song <eundoo.song@gmail.com>
2020-11-22 04:11:05 +0900

    Add initial version of data quality operator

--- 5.data-pipeline/airflow/plugins/operators/data_quality.py
+++ 5.data-pipeline/airflow/plugins/operators/data_quality.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+import logging
 
 
 class DataQualityOperator(BaseOperator):
@@ -16,4 +17,10 @@ class DataQualityOperator(BaseOperator):
         self.redshift_conn_id = redshift_conn_id
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        records = redshift.get_records("select count(*) from songs where songid = ''")
+        if len(records) > 0:
+            logging.warning("empty song id found in songs table")
+        records = redshift.get_records("select count(*) from artists where artistid = ''")
+        if len(records) > 0:
+            logging.warning("empty artist id found in artists table")
