dc1c864672e292c698827138f3b1085adeecb321
Eundoo Song <eundoo.song@gmail.com>
2020-11-25 04:18:29 +0900

    Fix wrong quotation mark

--- 5.data-pipeline/airflow/plugins/helpers/sql_queries.py
+++ 5.data-pipeline/airflow/plugins/helpers/sql_queries.py
@@ -1,5 +1,8 @@
 class SqlQueries:
-    songplays_table_insert = ("songplays", """"
+    """
+    Sql queries that insert data into fact, dimension table
+    """
+    songplays_table_insert = ("songplays", """
         INSERT INTO songplays (
             SELECT
                     md5(events.start_time) songplay_id,
@@ -21,7 +24,7 @@ class SqlQueries:
         )
     """)
 
-    users_table_insert = ("users", """"
+    users_table_insert = ("users", """
         INSERT INTO users (
             SELECT distinct userid, firstname, lastname, gender, level
             FROM staging_events
@@ -29,21 +32,21 @@ class SqlQueries:
         )
     """)
 
-    songs_table_insert = ("songs", """"
+    songs_table_insert = ("songs", """
         INSERT INTO songs (
             SELECT distinct song_id, title, artist_id, year, duration
             FROM staging_songs
         )
     """)
 
-    artists_table_insert = ("artists", """"
+    artists_table_insert = ("artists", """
         INSERT INTO artists (
             SELECT distinct artist_id, artist_name, artist_location, artist_latitude,
             artist_longitude FROM staging_songs
         )
     """)
 
-    time_table_insert = ("time", """"
+    time_table_insert = ("time", """
         INSERT INTO time (
             SELECT start_time, extract(hour from start_time), extract(day from start_time),
                 extract(week from start_time), extract(month from start_time),
