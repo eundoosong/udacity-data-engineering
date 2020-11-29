9a77c1e1e4f07c64faa383a9354d3d386219dc36
Eundoo Song <eundoo.song@gmail.com>
2020-11-30 01:41:40 +0900

    Fix catchup false

--- 5.data-pipeline/airflow/dags/pipeline.py
+++ 5.data-pipeline/airflow/dags/pipeline.py
@@ -14,13 +14,14 @@ default_args = {
     'retries': 3,
     'retry_delay': timedelta(minutes=5),
     'start_date': datetime(2019, 1, 12),
-    'catchup': False,
+    'catchup_by_default': False,
 }
 
 dag = DAG('udacity_pipeline',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
           schedule_interval='0 * * * *',
+          catchup=False,
           )
 
 with dag:
